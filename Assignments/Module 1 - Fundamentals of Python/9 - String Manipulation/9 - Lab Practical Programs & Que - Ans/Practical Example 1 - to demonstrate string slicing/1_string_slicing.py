4918084add43e8c351c4d0c4128d219b35543552
Richa Parmar <richaparmar0109@gmail.com>
2026-03-28 15:55:46 +0530

    commit

---
+++ Assignments/Module 1 - Fundamentals of Python/9 - String Manipulation/9 - Lab Practical Programs & Que - Ans/Practical Example 1 - to demonstrate string slicing/1_string_slicing.py
@@ -0,0 +1,11 @@
+str = "RichaParmar"
+
+print("Original String : ", str)
+
+print("first 6 characters : ", str[:6])
+
+print("From index 6 onwards: ", str[6:])
+
+print("characters between 2 to 6 :", str[2:6])
+
+print("Last character :", str[-1])
