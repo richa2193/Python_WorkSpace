c9d10913a6f0725f01d7526e2387f5c48ac5cd2e
Richa Parmar <richaparmar0109@gmail.com>
2026-03-28 15:55:22 +0530

    commit

---
+++ Assignments/Module 1 - Fundamentals of Python/8 - Control Statements - Break, continue, Pass/8 - Lab Practical Programs & Que - Ans/Practical Example 1 - to skip 'banana' in a list using the continue/1_continue_statement.py
@@ -0,0 +1,6 @@
+list = ['apple', 'banana', 'mango']
+
+for fruit in list:
+    if fruit == 'banana':
+        continue
+    print(fruit)
---
+++ Assignments/Module 1 - Fundamentals of Python/8 - Control Statements - Break, continue, Pass/8 - Lab Practical Programs & Que - Ans/Practical Example 2 - to stop the loop once 'banana' is found using the break/2_break_statement.py
@@ -0,0 +1,6 @@
+list = ['apple', 'banana', 'mango']
+
+for fruit in list:
+    if fruit == 'banana':
+        break
+    print(fruit)
