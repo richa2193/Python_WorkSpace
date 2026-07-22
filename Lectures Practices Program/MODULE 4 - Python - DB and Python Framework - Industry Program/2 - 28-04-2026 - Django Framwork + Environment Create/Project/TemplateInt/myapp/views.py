741ad07c919003a453e179fbea3d403ba61b36fe
Richa Parmar <richaparmar0109@gmail.com>
2026-07-22 12:16:55 +0530

    Commit

---
+++ Lectures Practices Program/MODULE 4 - Python - DB and Python Framework - Industry Program/2 - 28-04-2026 - Django Framwork + Environment Create/Project/TemplateInt/myapp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def service(request):
+    return render(request, 'service.html')
+
+def team(request):
+    return render(request, 'team.html')
+
+def testimonial(request):
+    return render(request, 'testimonial.html')
+
+def error(request):
+    return render(request, '404.html')
